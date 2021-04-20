b39634ce44ea639026064a5dbfa610745e4ce914
SheriffChen <loebmraz@gmail.com>
2021-04-20 11:40:55 +0800

    switch order && launch file

---
+++ Documentation/Basics/Flow/Switch evalution order/switch-evalution-order.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Print("When is Tuesday?")
+	today := time.Now().Weekday()
+	switch time.Tuesday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("Too far away")
+	}
+}
